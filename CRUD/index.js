e38fab1d1d261156205331599b693de0129f7b90
HipoTech <44259852+HipoTech@users.noreply.github.com>
2019-03-27 20:22:32 +0200

    updated

--- CRUD/index.js
+++ CRUD/index.js
@@ -1,36 +1,59 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
-const catSchema = new Schema({
-    Name: { type: String, required: true },
-    Age: { type: Number, required: true, min: 0 },
-    catId: { type: String, required: true }
-})
-
-
-const Cat = mongoose.model('Cat', catSchema)
+// const catSchema = new Schema({
+//     Name: { type: String, required: true },
+//     Age: { type: Number, required: true, min: 0 },
+//     catId: { type: String, required: true }
+// })
 
 
+// const Cat = mongoose.model('Cat', catSchema)
 
 
-mongoose
-    .connect('mongodb://localhost:27017/test', { useNewUrlParser: true })
-    .then(() => {
-        console.log('Database online!')
+// mongoose
+//     .connect('mongodb://localhost:27017/test', { useNewUrlParser: true })
+//     .then(() => {
+//         console.log('Database online!')
 
-        // Cat
-        //     .create({
-        //         Name: 'Ana',
-        //         Age: 3,
-        //         catId: 11111
-        //     })
-
-
-        Cat
-            .find()
-            .then((cats) => console.log(cats)
-            )
+//         Cat
+//             .create({
+//                 Name: 'Ana',
+//                 Age: 3,
+//                 catId: 11111
+//             })
 
 
-    });
+//         Cat
+//             .find()
+//             .then((cats) => console.log(cats)
+//             )
+
+
+//     });
+// ______________________________________________
+// const http = require('http');
+// http.createServer(function (req, res) {
+//     res.writeHead(200, { 'Contenc-Type': 'text/html' });
+//     res.write('<H1>Hellooooooooo<H1>');
+//     res.end();
+// }).listen(3000);
+// console.log('listening............');
+// ______________________________________________
+// const express = require('express');
+
+// const app = express();
+
+
+// app.get("/", function (req, res) {
+//     res.send('Helllooooooo madafacka');
+// })
+
+// app.get("/test", function (req, res) {
+//     res.send('Yea');
+// })
+
+// app.listen(3000, () => console.log("It's working!!!!")
+// )
+
