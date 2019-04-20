d5891798ee72b44cdf94ee96f7e14dd2a4cac947
HipoTech <44259852+HipoTech@users.noreply.github.com>
2019-04-20 13:10:45 +0300

    updated

--- Old exams/finalExam/Programming Fundamentals Exam - 01 July 2018 Part I/softUniReception.js
+++ Old exams/finalExam/Programming Fundamentals Exam - 01 July 2018 Part I/softUniReception.js
@@ -2,6 +2,7 @@ function reception(params) {
     let efficionsy = Number(params[0]) + Number(params[2]) + Number(params[1]);
     let students = Number(params[3]);
     let time = 0;
+
     time = Math.ceil(students / efficionsy);
     for (let i = 1; i <= time; i++) {
         if (i % 4 === 0) {
