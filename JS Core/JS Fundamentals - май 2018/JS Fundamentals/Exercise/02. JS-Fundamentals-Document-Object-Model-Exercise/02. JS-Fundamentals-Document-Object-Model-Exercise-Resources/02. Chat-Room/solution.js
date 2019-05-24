f16a63b216be88d7cd1e5b72a4268c3df3131c54
HipoTech <44259852+HipoTech@users.noreply.github.com>
2019-05-24 23:31:40 +0300

    added solutions

--- JS Core/JS Fundamentals - май 2018/JS Fundamentals/Exercise/02. JS-Fundamentals-Document-Object-Model-Exercise/02. JS-Fundamentals-Document-Object-Model-Exercise-Resources/02. Chat-Room/solution.js
+++ JS Core/JS Fundamentals - май 2018/JS Fundamentals/Exercise/02. JS-Fundamentals-Document-Object-Model-Exercise/02. JS-Fundamentals-Document-Object-Model-Exercise-Resources/02. Chat-Room/solution.js
@@ -1,5 +1,14 @@
 function solve() {
-   //TODO...
+   let btn = document.querySelectorAll('#send')[0];
+   let messageBox = document.querySelector('#chat_messages');
+   btn.addEventListener('click', sendMessage);
+
+   function sendMessage() {
+      let myMessage = document.getElementById('chat_input').value;
+      let div = document.createElement('div');
+      div.className = 'message my-message';
+      div.textContent = `${myMessage}`;
+      messageBox.appendChild(div);
+      document.querySelectorAll('#chat_input')[0].value = '';
+   }
 }
-
-
