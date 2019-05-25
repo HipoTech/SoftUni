a1b1f4dd8639f99a123c145254c11299329ef2c3
HipoTech <44259852+HipoTech@users.noreply.github.com>
2019-05-25 13:49:25 +0300

    added solutions

--- JS Core/JS Fundamentals - май 2018/JS Fundamentals/Exercise/02. JS-Fundamentals-Document-Object-Model-Exercise/02. JS-Fundamentals-Document-Object-Model-Exercise-Resources/03. Number-Convertor/solution.js
+++ JS Core/JS Fundamentals - май 2018/JS Fundamentals/Exercise/02. JS-Fundamentals-Document-Object-Model-Exercise/02. JS-Fundamentals-Document-Object-Model-Exercise-Resources/03. Number-Convertor/solution.js
@@ -1,4 +1,37 @@
 function solve() {
 
-    //TODO...
+    let convertBtn = document.querySelectorAll('#container button')[0];
+    let toList = document.querySelectorAll('#selectMenuTo')[0];
+    let optionToList = document.querySelector('#selectMenuTo option');
+    let inputOfUser = document.querySelector('#input');
+    let result = document.querySelector('#result');
+
+    addElementToList('binary', 'Binary');
+    addElementToList('hexadecimal', 'Hexadeicmal');
+
+    function addElementToList(valueOfNewProp, contentOfNewProp) {
+        let newElement = optionToList.cloneNode(true);
+        newElement.value = `${valueOfNewProp}`;
+        newElement.textContent = `${contentOfNewProp}`;
+        toList.appendChild(newElement);
+    }
+
+    convertBtn.addEventListener('click', convert);
+
+    function convert() {
+        if (toList.value === 'binary') {
+            convertBinary(inputOfUser.value)
+        } else if (toList.value === 'hexadecimal') {
+            convertHexadeicmal(inputOfUser.value)
+        }
+    }
+
+    function convertBinary(userValue) {
+        let binary = parseInt(userValue, 10);
+        result.value = binary.toString(2);
+    }
+
+    function convertHexadeicmal(userValue) {
+        result.value = Math.abs(userValue).toString(16).toUpperCase();
+    }
 }
