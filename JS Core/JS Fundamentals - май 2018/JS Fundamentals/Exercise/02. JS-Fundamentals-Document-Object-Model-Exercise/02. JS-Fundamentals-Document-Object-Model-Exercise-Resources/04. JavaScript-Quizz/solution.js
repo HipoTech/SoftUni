203023cd3049a130a8bf49c69f21126aab5eba3e
HipoTech <44259852+HipoTech@users.noreply.github.com>
2019-05-25 15:01:58 +0300

    added solutioans

--- JS Core/JS Fundamentals - май 2018/JS Fundamentals/Exercise/02. JS-Fundamentals-Document-Object-Model-Exercise/02. JS-Fundamentals-Document-Object-Model-Exercise-Resources/04. JavaScript-Quizz/solution.js
+++ JS Core/JS Fundamentals - май 2018/JS Fundamentals/Exercise/02. JS-Fundamentals-Document-Object-Model-Exercise/02. JS-Fundamentals-Document-Object-Model-Exercise-Resources/04. JavaScript-Quizz/solution.js
@@ -1,3 +1,40 @@
 function solve() {
-  //TODO...
+  let sections = document.querySelectorAll('section');
+
+  let correctCounter = 0;
+  let auizNumber = 0;
+
+  let ans = document.querySelectorAll('.quiz-answer p');
+  for (let i = 0; i < ans.length; i++) {
+    ans[i].addEventListener('click', quizAns);
+  }
+
+
+  function quizAns() {
+    console.log(this.textContent);
+    if (this.textContent === 'onclick' ||
+      this.textContent === 'JSON.stringify()' ||
+      this.textContent === 'A programming API for HTML and XML documents') {
+      correctCounter++;
+    }
+
+    sections[auizNumber].style = 'display:none'
+
+    if (auizNumber <= 1) {
+      sections[auizNumber + 1].style = 'display:block'
+    }
+    auizNumber++;
+    console.log(auizNumber);
+
+    if (auizNumber === 3) {
+      let result = document.querySelector('#results');
+      let resultText = document.querySelector('#results .results-inner h1');
+      result.style = 'display:block';
+      if (correctCounter === 3) {
+        resultText.textContent = 'You are recognized as top JavaScript fan!';
+      } else {
+        resultText.textContent = `You have ${correctCounter} right answers`;
+      }
+    }
+  }
 }
