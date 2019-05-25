a6f9782591653282e1161f5f30552c1cd9d5bbd9
HipoTech <44259852+HipoTech@users.noreply.github.com>
2019-05-25 17:01:22 +0300

    added solution

--- JS Core/JS Fundamentals - май 2018/JS Fundamentals/Exercise/02. JS-Fundamentals-Document-Object-Model-Exercise/02. JS-Fundamentals-Document-Object-Model-Exercise-Resources/05. Table-Search-Engine/solution.js
+++ JS Core/JS Fundamentals - май 2018/JS Fundamentals/Exercise/02. JS-Fundamentals-Document-Object-Model-Exercise/02. JS-Fundamentals-Document-Object-Model-Exercise-Resources/05. Table-Search-Engine/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-   //TODO...
+   let userInput = document.querySelector('#searchField');
+   let rows = document.querySelectorAll('tbody tr');
+   let search = document.querySelector('#searchBtn');
+
+   search.addEventListener('click', searchRows);
+
+   function searchRows() {
+      let searchFor = userInput.value;
+
+      for (let i = 0; i < rows.length; i++) {
+         rows[i].className = 'searchIt';
+         let data = document.querySelectorAll('.searchIt td');
+         let flag = false;
+         for (let j = 0; j < data.length; j++) {
+            if (data[j].textContent.includes(searchFor)) {
+               flag = true;
+            }
+
+         }
+         if (flag) {
+            rows[i].className = 'select';
+         } else {
+            rows[i].removeAttribute('class')
+         }
+      }
+   }
 }
