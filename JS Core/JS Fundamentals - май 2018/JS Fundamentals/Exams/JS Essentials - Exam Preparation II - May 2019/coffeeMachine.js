3a82e4fc0afaef1bdf1187c399a860a9b2ae14b3
HipoTech <44259852+HipoTech@users.noreply.github.com>
2019-06-08 15:38:36 +0300

    update

--- JS Core/JS Fundamentals - май 2018/JS Fundamentals/Exams/JS Essentials - Exam Preparation II - May 2019/coffeeMachine.js
+++ JS Core/JS Fundamentals - май 2018/JS Fundamentals/Exams/JS Essentials - Exam Preparation II - May 2019/coffeeMachine.js
@@ -2,13 +2,65 @@ function coffeeMachine(inputArea) {
     const caffeeCaffeinePrice = 0.8;
     const caffeeDecafPrice = 0.9;
     const teaPrice = 0.8;
+    let totalPrice = 0;
+    let extraPrice = 0;
+    let sugarPrice = 0;
 
     inputArea.forEach(order => {
         const orderSplited = order.split(', ');
-        if (orderSplited.length) {
+        const money = orderSplited.shift();
+        const drink = orderSplited.shift();
+        let sugar = 0;
+        if (drink === 'coffee') {
+            const type = orderSplited.shift();
+            if (type === 'caffeine') {
+                totalPrice += caffeeCaffeinePrice;
+            } else if (type === 'decaf') {
+                totalPrice += caffeeDecafPrice;
+            }
+
+
+            if (orderSplited.length === 2) {
+                const extra = orderSplited.shift();
+                sugare = orderSplited.shift();
+
+                if (extra === 'milk') {
+                    extraPrice = Number((totalPrice * 0.1).toFixed(2));
+                }
+                if (sugare > 0) {
+                    sugarPrice = 0.1;
+                }
+            } else if (orderSplited.length === 1) {
+                sugare = orderSplited.shift();
+
+                if (sugare > 0) {
+                    sugarPrice = 0.1;
+                }
+            }
+            console.log((totalPrice + extraPrice + sugarPrice));
+        } else {
+            totalPrice = teaPrice;
+
+            if (orderSplited.length === 2) {
+                const extra = orderSplited.shift();
+                sugare = orderSplited.shift();
+
+                if (extra === 'milk') {
+                    extraPrice = Number((totalPrice * 0.1).toFixed(2));
+                }
+                if (sugare > 0) {
+                    sugarPrice = 0.1;
+                }
+            } else if (orderSplited.length === 1) {
+                sugare = orderSplited.shift();
+
+                if (sugare > 0) {
+                    sugarPrice = 0.1;
+                }
+            }
+            console.log((totalPrice + extraPrice + sugarPrice));
 
         }
-        const [moneyIn, drink, type, extra, sugar]
     });
 
 }
