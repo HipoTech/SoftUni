9117cfef7dd0c2a3b3ccaa779541104c4e600d92
HipoTech <44259852+HipoTech@users.noreply.github.com>
2019-07-28 18:19:59 +0300

    added solutions

--- JS Core/JS Apps/05. JS-Applications-Templating-Exercise/02. HTTP Status Cats/template.js
+++ JS Core/JS Apps/05. JS-Applications-Templating-Exercise/02. HTTP Status Cats/template.js
@@ -1,8 +1,44 @@
 (() => {
-     renderCatTemplate();
+    renderCatTemplate();
 
-     function renderCatTemplate() {
-         // TODO: Render cat template and attach events
-     }
- 
-})
+    function renderCatTemplate() {
+        const determinateElement = function (ctx) {
+            const btns = document.querySelectorAll('.showBtn');
+            btns.forEach(btn => {
+                btn.addEventListener('click', showStatus)
+            });
+
+        }
+
+        const showStatus = function () {
+            const statusToShow = this.parentNode
+                .querySelector('.status')
+                .style.display
+            if (statusToShow === 'none') {
+                this.parentNode.querySelector('.status').style.display = 'block'
+            } else {
+                this.parentNode.querySelector('.status').style.display = 'none'
+            }
+
+        }
+        fetch('template.hbs')
+            .then(response => response.text())
+            .then((data) => {
+                let source = data;
+                renderTemplate(source);
+            })
+            .then(() => determinateElement())
+            .catch((error) => {
+                console.log(error);
+            });
+
+        const renderTemplate = function (source) {
+            const root = document.querySelector('#allCats ul');
+            const compileed = Handlebars.compile(source);
+            const rendered = compileed({ cats });
+            root.innerHTML = rendered;
+        }
+    }
+
+
+})()
--- JS Core/JS Apps/05. JS-Applications-Templating-Exercise/03. Popular Monkeys/monkeysTemplate.js
+++ JS Core/JS Apps/05. JS-Applications-Templating-Exercise/03. Popular Monkeys/monkeysTemplate.js
@@ -1,3 +1,38 @@
-$(() => {
-    // TODO
-})
+(() => {
+    const determinateElement = function (ctx) {
+        const btns = document.querySelectorAll('button');
+        btns.forEach(btn => {
+            btn.addEventListener('click', showStatus)
+        });
+
+    }
+
+    const showStatus = function () {
+        const statusToShow = this.parentNode
+            .querySelector('p')
+            .style.display
+        if (statusToShow === 'none') {
+            this.parentNode.querySelector('p').style.display = 'block'
+        } else {
+            this.parentNode.querySelector('p').style.display = 'none'
+        }
+
+    }
+    fetch('template.hbs')
+        .then(response => response.text())
+        .then((data) => {
+            let source = data;
+            renderTemplate(source);
+        })
+        .then(() => determinateElement())
+        .catch((error) => {
+            console.log(error);
+        });
+
+    const renderTemplate = function (source) {
+        const root = document.querySelector('.monkeys');
+        const compileed = Handlebars.compile(source);
+        const rendered = compileed({ monkeys });
+        root.innerHTML = rendered;
+    }
+})()
