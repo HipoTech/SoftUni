e2fcd569524a5dbaa105e5d8da3aa7ed5c14db60
HipoTech <44259852+HipoTech@users.noreply.github.com>
2019-10-01 18:42:16 +0300

    updated

--- JS Web/3. EXPRESSJS AND TEMPLATING/WORKSHOP EXPRESSJS AND TEMPLATING/03. Cubicle-Workshop-Part-1-Resources/controllers/createCube.js
+++ JS Web/3. EXPRESSJS AND TEMPLATING/WORKSHOP EXPRESSJS AND TEMPLATING/03. Cubicle-Workshop-Part-1-Resources/controllers/createCube.js
@@ -1,20 +1,39 @@
 const Cube = require('../models/cube').Cube;
 const fs = require('fs');
-const data = require('../config/database');
-const len = require('../config/database').length;
+const path = require('path');
+const dbFile = path.resolve(__projectDir, 'config/database.json');
+
+const _readDb = async function () {
+    await fs.readFile(dbFile, 'utf8', (err, data) => {
+        if (err) {
+            throw new Error(err);
+        };
+        dbData = data;
+    });
+}
+
+let dbData = _readDb();
+
+const _writeDb = function (data) {
+    fs.writeFile(dbFile, data, (err) => {
+        if (err) {
+            throw new Error(err);
+        };
+    });
+}
 
 const createCube = function (req, res) {
-    const { name, description, imageUrl, difficultyLevel } = req.body;
-    console.log(name);
-    console.log(description);
-    console.log(imageUrl);
-    console.log(difficultyLevel);
-    const newCube = new Cube(name, description, imageUrl, difficultyLevel);
 
-    data.push(newCube);
-    res.redirect('/');
-}
+    dbData = JSON.parse(dbData);
+    const { name, description, imageUrl, difficultyLevel } = req.body;
+    const id = dbData.length + 1;
+    const newCube = new Cube(id, name, description, imageUrl, difficultyLevel);
+    dbData.push(newCube);
+    //_writeDb(dbData);
+    //res.redirect('/');
+};
 
 module.exports = {
     createCube,
+
 }
--- JS Web/3. EXPRESSJS AND TEMPLATING/WORKSHOP EXPRESSJS AND TEMPLATING/03. Cubicle-Workshop-Part-1-Resources/models/cube.js
+++ JS Web/3. EXPRESSJS AND TEMPLATING/WORKSHOP EXPRESSJS AND TEMPLATING/03. Cubicle-Workshop-Part-1-Resources/models/cube.js
@@ -1,8 +1,6 @@
-const len = require('../config/database').length;
-
 class Cube {
-    constructor(name, description, imageUrl, difficultyLevel) {
-        this.id = len + 1;
+    constructor(id, name, description, imageUrl, difficultyLevel) {
+        this.id = id;
         this.name = name;
         this.description = description;
         this.imageUrl = imageUrl;
