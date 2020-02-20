315063f8c4490b1063baf417d4df3b259b9e84a0
HipoTech <44259852+HipoTech@users.noreply.github.com>
2020-02-20 22:03:29 +0200

    detail animation added

--- Front-End Frameworks/Angular/ecommerce-Front-End/e-commerce/src/app/core/product-derail/product-derail.component.ts
+++ Front-End Frameworks/Angular/ecommerce-Front-End/e-commerce/src/app/core/product-derail/product-derail.component.ts
@@ -1,17 +1,44 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Product, Brand } from 'src/app/shared/interfaces';
+import { trigger, state, style, animate, transition } from '@angular/animations'
+
+import { Product } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-product-derail',
   templateUrl: './product-derail.component.html',
-  styleUrls: ['./product-derail.component.scss']
+  styleUrls: ['./product-derail.component.scss'],
+  animations: [
+    trigger('fadeInState', [
+      state('start', style({
+        opacity: 0
+      })),
+      state('end', style({
+        opacity: 1
+      })),
+      transition('start => end', animate('2000ms ease-out')),
+    ])
+  ]
 })
 export class ProductDerailComponent implements OnInit {
   @Input() product: Product;
+  show = true;
 
   constructor() { }
 
+  get stateName() {
+    return this.show ? 'start' : 'end'
+  }
+
+  startAnimation() {
+    this.show = false;
+  }
+
   ngOnInit() {
+    setTimeout(() => {
+      this.startAnimation()
+      console.log('yes');
+
+    }, 300);
   }
 
 }
